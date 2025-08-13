62eab974b40589861770607f3fb7ce647f6a540c
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-08-14 03:17:29 +0530

    prisma usecase

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,11 +1,17 @@
+import { PrismaClient } from "@/app/generated/prisma";
 import { NextRequest } from "next/server";
-
+const client = new PrismaClient();
 export async function POST(req: NextRequest) {
     // extract the body
     const body = await req.json();
     console.log(body);
     // store the body in the DB
-
+    await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password,
+        },
+    });
     return Response.json({
         msg: "User created successfully",
     });
