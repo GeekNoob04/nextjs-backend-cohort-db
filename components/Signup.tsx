d6ad6b3e607d4ca2781e97c18c8cf6a2c125eee4
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-08-18 02:24:57 +0530

    useRouter

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { signup } from "@/app/actions/user";
 // import axios from "axios";
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { ChangeEventHandler, useState } from "react";
 export function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    // const router = useRouter();
+    const router = useRouter();
 
     return (
         <div className="h-screen flex justify-center flex-col">
@@ -44,7 +44,7 @@ export function Signup() {
                                         password
                                     );
                                     console.log(res);
-                                    // router.push("/");
+                                    router.push("/");
                                 }}
                                 type="button"
                                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
